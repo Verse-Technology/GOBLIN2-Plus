9363193d013708936b574eb47247b207141cd26e
GOBLIN2PLUS <31169904+GOBLIN2PLUS@users.noreply.github.com>
2017-08-19 17:57:21 -0500

    Add files via upload

---
+++ BRIDGE_GOBLIN2PLUS/BRIDGE_GOBLIN2PLUS.ino
@@ -0,0 +1,35 @@
+/*     BRIDGE_GOBLIN2PLUS.
+
+  WITH THIS PROGRAM YOU CAN HAVE COMMUNICATION BETWEEN SIM5320A AND YOUR ARDUINO IDE.
+  
+  Check more documentation and examples in www.goblin2plus.io
+ 
+  modified 4 August 2017.
+  by Jorge Guillen, Antonio Reyes & Rafa Cortés.
+  -GOBLIN 2 Plus TEAM. VERSE Technology.
+*/
+
+void setup() {
+  SerialUSB.begin(115200);              // DEFAULT BAUDS 115200 of SIM5320A
+  Serial.begin(115200);                 // SERIAL - SIM
+  while (!SerialUSB);
+}
+
+void loop() {
+  
+  if (Serial.available()) {   
+      String toSend = ""; 
+      while (Serial.available())  {   
+      toSend += (char)Serial.read();
+                                      }
+      SerialUSB.print(toSend);
+                                    }  
+
+  if (SerialUSB.available()) {   
+      String toSend = ""; 
+      while (SerialUSB.available())  {   
+      toSend += (char)SerialUSB.read();
+                                      }
+      Serial.print(toSend);
+  }
+}
